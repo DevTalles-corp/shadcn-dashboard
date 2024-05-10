306609da11e6f80d41b5a98a0e578efd34a4e200
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-10 15:47:15 -0400

    feat: add card link to dashboard layout

---
+++ src/app/dashboard/card/page.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {"123456789".split("").map((item) => (
+        <Card key={item}>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="ghost">info</Button>
+            <Button>más</Button>
+          </CardFooter>
+        </Card>
+      ))}
+
+      <Card className="col-span-1 sm:col-span-3">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,6 +13,7 @@ const links = [
   { name: "badge", href: "badge" },
   { name: "calendar", href: "calendar" },
   { name: "avatar", href: "avatar" },
+  { name: "card", href: "card" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
