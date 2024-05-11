9da7552f4c74bd505a817ec4348181c5e90e0ca2
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-11 09:37:19 -0400

    Menu Bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,6 +19,7 @@ const links = [
   { name: "command", href: "command" },
   { name: "combobox", href: "combobox" },
   { name: "context menu", href: "context-menu" },
+  { name: "menu bar", href: "menu-bar" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -160,7 +161,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden ">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
@@ -272,7 +273,7 @@ export default function DashboardLayout({
               </a>
             </div>
           </footer>
-          <p className="text-center text-sm text-gray-500 my-10">
+          <p className="text-center text-sm text-gray-500 my-10 print:hidden">
             &copy; 2019-{new Date().getFullYear()}{" "}
             <a href="#" className="hover:underline" target="_blank">
               Themesberg
