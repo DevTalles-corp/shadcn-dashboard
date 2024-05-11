c9b73ec246fa986d879ba48216da17aa0bde6e1d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-11 09:20:35 -0400

    feat: add status selection to combobox page

--- src/app/dashboard/combobox/page.tsx
+++ src/app/dashboard/combobox/page.tsx
@@ -12,6 +12,7 @@ import {
   CommandGroup,
   CommandInput,
   CommandItem,
+  CommandList,
 } from "@/components/ui/command";
 import {
   Popover,
@@ -42,12 +43,45 @@ const frameworks = [
   },
 ];
 
+// Ejercicio #2
+type Status = {
+  value: string;
+  label: string;
+};
+
+const statuses: Status[] = [
+  {
+    value: "backlog",
+    label: "Backlog",
+  },
+  {
+    value: "todo",
+    label: "Todo",
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+  },
+  {
+    value: "done",
+    label: "Done",
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+  },
+];
+
 export default function Page() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
+  // Ejercicio #2
+  const [open2, setOpen2] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+
   return (
-    <div>
+    <div className="grid grid-cols-2">
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
@@ -65,30 +99,72 @@ export default function Page() {
         <PopoverContent className="w-[200px] p-0">
           <Command>
             <CommandInput placeholder="Search framework..." className="h-9" />
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {frameworks.map((framework) => (
-                <CommandItem
-                  key={framework.value}
-                  value={framework.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  {framework.label}
-                  <CheckIcon
-                    className={cn(
-                      "ml-auto h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
+
+            <CommandList>
+              <CommandEmpty>No framework found.</CommandEmpty>
+              <CommandGroup>
+                {frameworks.map((framework) => (
+                  <CommandItem
+                    key={framework.value}
+                    value={framework.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    {framework.label}
+                    <CheckIcon
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        value === framework.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
           </Command>
         </PopoverContent>
       </Popover>
+
+      {/* Ejercicio #2 */}
+
+      <div className="flex items-center space-x-4">
+        <p className="text-sm text-muted-foreground">Status</p>
+        <Popover open={open2} onOpenChange={setOpen2}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="w-[150px] justify-start">
+              {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-0" side="right" align="start">
+            <Command>
+              <CommandInput placeholder="Change status..." />
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup>
+                  {statuses.map((status) => (
+                    <CommandItem
+                      key={status.value}
+                      value={status.value}
+                      onSelect={(value) => {
+                        setSelectedStatus(
+                          statuses.find(
+                            (priority) => priority.value === value
+                          ) || null
+                        );
+                        setOpen2(false);
+                      }}
+                    >
+                      {status.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 }
